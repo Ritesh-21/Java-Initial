00a892be05ece751d8a90d3b13cfa0eff81e3830
Ritesh Rajput <142082426+Ritesh-21@users.noreply.github.com>
2025-12-09 21:12:40 +0530

    Add files via upload

---
+++ Java-Through-Pw/Module-18/Tut222.java
@@ -0,0 +1,36 @@
+// interface A {
+//     void show();
+
+//     default void config() // DEFAULT use must
+//     {
+//         System.out.println("This is Configg ");
+
+//     }
+
+//     static void abc() {
+
+//         System.out.println("ABC is here ");
+//     }
+
+// }
+
+// class teppa implements A {
+
+//   public  void show(){
+// System.out.println("is this bigg show ");
+//     }
+
+
+
+// }
+
+// public class Tut222 {
+//     public static void main(String[] args) {
+//         A.abc();
+//         teppa tt = new teppa();
+//         tt.config();
+//         tt.show();
+        
+
+//     }
+// }
---
+++ Java-Through-Pw/Module-18/Tut223.java
@@ -0,0 +1,31 @@
+// class A {
+
+//     public void show(){
+// System.out.println("Job Chahii Hamka ");
+
+//     }
+// static class B {
+// public void display(){
+//     System.out.println("Job khatirr Padna padatt");
+// }
+
+// }
+
+
+// }
+
+// public class Tut223 {
+//     public static void main(String[] args) {
+//         // Inner classes are of three type
+//         // MEMBER   STATIC   ANONYMOUS 
+//         A aa = new A();
+//         A.B bb = new A.B(); // If Static 
+//         // A.B bb = aa.new B(); // if MEMBER 
+//         aa.show();
+//         bb.display();
+        
+        
+        
+//     }
+    
+// }
