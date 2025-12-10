3690bedb9b181537803ff1c425a8d26ffc0cfb30
Ritesh Rajput <142082426+Ritesh-21@users.noreply.github.com>
2025-12-10 20:48:34 +0530

    Add files via upload

---
+++ Java-Through-Pw/Module-19/Tut4.java
@@ -0,0 +1,36 @@
+// import java.util.TreeSet;
+
+public class Tut4 {
+
+    // TREE-SET 
+
+    public static void main(String[] args) {
+        
+        // TreeSet ts = new TreeSet<>();
+
+        // ts.add(25);
+        // ts.add(12);
+        // ts.add(75);
+        // ts.add(200);
+        // ts.add(87);
+        // ts.add(43);
+        // ts.add(98);
+
+        // System.out.println(ts);
+
+        // ts.add(14);
+
+        // System.out.println(ts.higher(45));
+        // System.out.println(ts.lower(55));
+
+        // System.out.println(ts.ceiling(70));
+        // System.out.println(ts.floor(89));
+
+        // // ts.add(200); Duplicasy is not allowed 
+
+        // // System.out.println(ts);
+
+
+    }
+    
+}
