d02e7ce8047d199f18993bb1b6e9eba7d646cabe
Ritesh Rajput <142082426+Ritesh-21@users.noreply.github.com>
2025-12-11 22:35:33 +0530

    Add files via upload

---
+++ Java-Through-Pw/Module-19/Tut8.java
@@ -0,0 +1,32 @@
+// import java.util.HashMap;
+// import java.util.Hashtable;
+
+public class Tut8 {
+
+    // HASH-TABLE 
+
+    public static void main(String[] args) {
+        // Hashtable ht = new Hashtable();
+
+        // ht.put(21, "Ritti");
+        // ht.put(31, "Clouds");
+        // ht.put(01, "January");
+        // ht.put(null, "January");
+
+        // ht.putIfAbsent(21, "Rittii");
+
+        // System.out.println(ht);
+
+        System.out.println("HASH-MAP");
+
+        // HashMap hm = new HashMap();
+        // hm.put(21, "March");
+        // hm.put(31, "May");
+        // hm.put(14, "Novemember");
+        // hm.put(22, "Novemember");
+        // hm.put(44, "Novemember");
+
+        // System.out.println(hm);
+    }
+    
+}
