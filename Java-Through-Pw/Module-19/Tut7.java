e767eca8a3d839cdf2613192541380b15b14c522
Ritesh Rajput <142082426+Ritesh-21@users.noreply.github.com>
2025-12-11 22:33:57 +0530

    Add files via upload

---
+++ Java-Through-Pw/Module-19/Tut7.java
@@ -0,0 +1,33 @@
+// import java.util.HashMap;
+// import java.util.LinkedHashMap;
+
+public class Tut7 {
+
+    // HASH-MAP 
+
+    public static void main(String[] args) {
+
+        // HashMap hm = new HashMap();
+        // hm.put(31, "Clouds"); // Entry
+        // hm.put(21, "Clowns");
+        // // hm.put(21, "Clowns");
+
+        // System.out.println(hm);
+
+        // HashMap hm2 = new HashMap();
+        // hm2.put("Badal", "Clouds"); 
+        // hm2.put("Ritti", "Clown"); 
+        // System.out.println(hm2);
+
+        // LinkedHashMap hm3 = new LinkedHashMap();
+        // hm3.put("Badal", "Clouds"); 
+        // hm3.put("Ritti", "Clown"); 
+
+        // System.out.println(hm3);
+
+
+    
+        
+    }
+    
+}
