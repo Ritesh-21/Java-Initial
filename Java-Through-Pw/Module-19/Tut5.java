89b9b11e6cdea221d236020e36c481e3e889708d
Ritesh Rajput <142082426+Ritesh-21@users.noreply.github.com>
2025-12-11 22:32:15 +0530

    Add files via upload

---
+++ Java-Through-Pw/Module-19/Tut5.java
@@ -0,0 +1,28 @@
+// import java.util.HashSet;
+// import java.util.LinkedHashSet;
+
+public class Tut5 {
+
+    // HASH-SET 
+    // public static void main(String[] args) {
+
+    //     HashSet hs = new HashSet();
+    //     hs.add(45);
+    //     hs.add(76);
+    //     hs.add(87);
+    //     hs.add(23);
+
+    //     System.out.println(hs);
+
+    //     LinkedHashSet hs2 = new LinkedHashSet();
+
+    //     hs2.add(22);
+    //     hs2.add(11);
+    //     hs2.add(8);
+    //     hs2.add(88);
+
+    //     System.out.println(hs2);
+        
+    }
+    
+// }
