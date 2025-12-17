c377f0c7530914a57090178b94b0d13a9d5b4d88
Ritesh Rajput <142082426+Ritesh-21@users.noreply.github.com>
2025-12-17 20:19:49 +0530

    Add files via upload

---
+++ Java-Through-Pw/Module-20/Tut3.java
@@ -0,0 +1,62 @@
+// import java.util.ArrayList;
+// import java.util.Collections;
+
+public class Tut3 {
+
+    // Collectons Class and its inBuilt Method 
+
+    public static void main(String[] args) {
+        // ArrayList al = new ArrayList();
+
+        // al.add(21);
+        // al.add(31);
+        // al.add(14);
+        // al.add(19);
+        // al.add(24);
+        // al.add(30);
+
+        // System.out.println(al);
+
+        // Collections.sort(al);
+        // System.out.println(al);
+
+        // ArrayList al2 = new ArrayList();
+
+        // al2.add("Ritti");
+        // al2.add("Mickey");
+        // al2.add("Clouds");
+        // al2.add("Badal");
+        // al2.add("Chotti");
+        
+        // System.out.println(al2);
+
+        // Collections.sort(al2);
+        // System.out.println(al2);
+
+
+        // ArrayList al3 = new ArrayList();
+
+        // al3.add(10);
+        // al3.add(20);
+        // al3.add(30);
+        // al3.add(40);
+        // al3.add(50);
+        // al3.add(60);
+        // al3.add(60);
+        // System.out.println(al3);
+
+        // int index = Collections.binarySearch(al3, 30);
+        // System.out.println(index);
+
+        // Collections.rotate(al3, 3);
+        // System.out.println(al3);
+
+        // Collections.shuffle(al3);
+        // System.out.println(al3);
+
+        // System.out.println(Collections.frequency(al3, 60));
+
+       
+    }
+    
+}
