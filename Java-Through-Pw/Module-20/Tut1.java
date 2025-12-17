2ade258cad6d159b100172b371bb73b88267d7df
Ritesh Rajput <142082426+Ritesh-21@users.noreply.github.com>
2025-12-17 20:18:00 +0530

    Add files via upload

---
+++ Java-Through-Pw/Module-20/Tut1.java
@@ -0,0 +1,43 @@
+// import java.util.ArrayList;
+
+public class Tut1 {
+    
+    // Need of Generics and Basics of Generics 
+
+    public static void main(String[] args) {
+
+        String st [] = new String[10];
+
+        // TypeSafety 
+
+        st[0]="Dino james";
+        st[1]="Badshaa";
+        st[2]="Ikka ";
+        // st[3]=21;
+
+        // String name1 = st[0];
+        // String name2 = st[1];
+
+        // No TypeSafety
+        // TypeSafety achived using generics 
+        // ArrayList<String> al = new ArrayList<String>();
+
+        // al.add("Dino James");
+        // al.add("ikka");
+
+        // al.add(2131);
+
+        // String n1 = (String)al.get(0); //TypeCasting 
+        // String n2 = ( String )al.get(1);
+        // String n3 = ( String )al.get(3);
+
+
+        // String n1 = ( String )al.get(1);
+        // String n2 = ( String )al.get(2);
+
+
+
+
+        
+    }
+}
