4ccc9c162bed2d5332109b0c1b002b72893bc39b
Ritesh Rajput <142082426+Ritesh-21@users.noreply.github.com>
2025-12-17 20:19:19 +0530

    Add files via upload

---
+++ Java-Through-Pw/Module-20/Tut2.java
@@ -0,0 +1,45 @@
+// import java.util.ArrayList;
+// import java.util.Collection;
+// import java.util.List;
+// class Gen<T> {
+// T obj ;
+
+// public Gen(T obj){
+
+//     this.obj = obj;
+// }
+// void disp(){
+//     System.out.println("The type of data is"+obj.getClass().getName());
+// }
+
+// public T getobj(){
+//     return obj;
+// }
+
+// }
+// public class Tut2 {
+
+//     // More on Genrics in JAVA 
+
+//     public static void main(String[] args) {
+
+//         // ArrayList<String> list = new ArrayList<String>();
+//         // List<String> list1 = new ArrayList<String>();
+//         // Collection<Integer> list2 = new ArrayList<Integer>();
+
+//         // List<Object> list4 = new ArrayList<String>();  Compile time error
+
+//         // List<int> list4 = new ArrayList<int>();  Primitive value not allowed
+
+//         Gen<Integer> g = new Gen<Integer>(10);
+
+//         g.disp();
+//         System.out.println(g.getobj());
+
+
+
+
+        
+//     }
+    
+// }
