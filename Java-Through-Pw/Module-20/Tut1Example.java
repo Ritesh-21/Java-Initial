650f3d657848bd746bb33c1ab8a7b9433b4ed6f8
Ritesh Rajput <142082426+Ritesh-21@users.noreply.github.com>
2025-12-17 20:18:47 +0530

    Add files via upload

---
+++ Java-Through-Pw/Module-20/Tut1Example.java
@@ -0,0 +1,38 @@
+// import java.util.ArrayList;
+
+// class Student1 {
+
+//     private String name;
+//     private int id ;
+// }
+
+// class employe1{
+
+//     private String name ;
+//     private int id;
+// }
+
+
+
+
+
+
+// public class Tut1Example { 
+//     // Example of GeNERICS 
+//     public static void main(String[] args) {
+
+//         Student1 st = new Student1();
+//         Student1 st2 = new Student1();
+
+//         employe1 emp = new employe1();
+
+
+//         ArrayList<Student1> al = new ArrayList<Student1>();
+//         al.add(st);
+//         al.add(st2);
+
+//         // al.add(emp);
+        
+//     }
+    
+// }
