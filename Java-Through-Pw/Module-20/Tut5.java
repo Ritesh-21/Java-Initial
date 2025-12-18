fa21ba037b2ec7549ecc587108ae0b6039fc9423
Ritesh Rajput <142082426+Ritesh-21@users.noreply.github.com>
2025-12-18 20:19:40 +0530

    Add files via upload

---
+++ Java-Through-Pw/Module-20/Tut5.java
@@ -0,0 +1,36 @@
+// class Student {
+
+//     private String name;
+//     private int age;
+//     private String city;
+
+
+//     public Student(int age , String name , String city){
+
+//         this.age = age;
+//         this.name = name;
+//         this.city = city;
+
+
+//     }
+
+//     public String toString(){
+//         return " Name:" + name +" " + "Age:" + age +" "+ "City:" + city;
+//     }
+
+// }
+
+// public class Tut5 {
+
+//     // Fundamental Before IO Operation 
+//     public static void main(String[] args) {
+
+//         Student St1 = new Student(21, "Tejas", "Delhi");
+//         Student St2 = new Student(22, "Sukhoi", "Benglore");
+
+//         System.out.println(St1);
+//         System.out.println(St2);
+        
+//     }
+    
+// }
