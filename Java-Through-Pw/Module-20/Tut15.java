962af3e79da4ba504cbf2e48e9fcf77494c74967
Ritesh Rajput <142082426+Ritesh-21@users.noreply.github.com>
2025-12-26 20:12:49 +0530

    Add files via upload

---
+++ Java-Through-Pw/Module-20/Tut15.java
@@ -0,0 +1,52 @@
+// import java.io.Serializable;
+
+// class Cricketer implements Serializable {
+
+//      String name;
+//     transient  int run;
+//      int age;
+
+//     public Cricketer(int run, String name, int age) {
+//         this.name = name;
+//         this.run = run;
+//         this.age = age;
+
+//     }
+
+//     public void disp(){
+
+//         System.out.println(name);
+//         System.out.println(age);
+//         System.out.println(run);
+//     }
+// }
+// public class Tut15 {
+
+//     public static void main(String[] args) throws Exception
+//     {
+
+//     //    Cricketer cr = new Cricketer(31, "Ballu", 21);
+       
+//     //    FileOutputStream fos = new FileOutputStream("pw.extt"); // SERIALIZATION 
+//     //    BufferedOutputStream bos = new BufferedOutputStream(fos);
+//     //    ObjectOutputStream oos = new ObjectOutputStream(bos);
+
+//     //    oos.writeObject(cr);
+       
+//     //    oos.flush();
+//     //    oos.close();
+       
+//     //    FileInputStream fis = new FileInputStream("pw.extt");
+//     //    BufferedInputStream bis = new BufferedInputStream(fis); // DESERIALIZATION with transient 
+//     //    ObjectInputStream ois = new ObjectInputStream(bis);
+       
+//     //    Cricketer cr = (Cricketer)ois.readObject();
+//     //    cr.disp();
+
+       
+        
+//     }
+
+    
+    
+// }
