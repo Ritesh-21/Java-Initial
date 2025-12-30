1977123db69f30c80631dde3804fe51c6f9da077
Ritesh Rajput <142082426+Ritesh-21@users.noreply.github.com>
2025-12-30 21:39:36 +0530

    Add files via upload

---
+++ Java-Through-Pw/Module-21/Tut2.java
@@ -0,0 +1,29 @@
+// import java.time.*;
+public class Tut2 {
+
+    public static void main(String[] args) {
+        
+        // More on Date And Time ApI 
+
+        // LocalDate date = LocalDate.now();
+        // System.out.println(date);
+
+        // int dd =date.getMonthValue();
+
+        // int dd1 = date.getDayOfMonth();
+
+        // int dd2 = date.getYear();
+
+        // System.out.println(dd1 + "/"+ dd + "/"+dd2);
+
+
+        // LocalTime time = LocalTime.now();
+
+        // System.out.println(time);
+
+        // int hour = time.getHour();
+        // System.out.println(hour);
+
+    }
+    
+}
