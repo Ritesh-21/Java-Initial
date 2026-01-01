b6480587663460eae63c91f3a815612469a50d07
Ritesh Rajput <142082426+Ritesh-21@users.noreply.github.com>
2026-01-01 18:26:00 +0530

    Add files via upload

---
+++ Java-Through-Pw/Module-21/Tut5.java
@@ -0,0 +1,32 @@
+// import java.util.Arrays;
+// import java.util.List;
+// import java.util.stream.Stream;
+
+public class Tut5 { 
+    public static void main(String[] args) {
+
+        // More on Stream Api 
+        // List<Integer> list =  Arrays.asList(2,5,4,8,6);
+
+        // Stream<Integer> std=list.stream();
+
+    //    long dc= std.count();
+    //     System.out.println(dc);
+
+    // Stream ffdt=std.filter(n->n%2==0).sorted().map(n->n*2); // saron ko ek sath call kiya kyuki sabka 
+    // written type same tha 
+
+    // Stream<Integer> sstd= fdt.sorted();
+    // Stream<Integer> sstd= std.sorted();
+
+        // Stream<Integer> sstdd=sstd.map(n->n*2);
+
+        // ffdt.forEach(n->System.out.println(n));
+        // sstd.forEach(n->System.out.println(n));
+        // std.forEach(n->System.out.println(n));
+
+
+        
+    }
+    
+}
